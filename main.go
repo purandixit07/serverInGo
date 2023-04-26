f571d05d483335f764950f3daeb46dfc810d953e
Puran Singh <purandixit@gmail.com>
2023-04-26 13:05:04 +0530

    added handlerFunc

--- main.go
+++ main.go
@@ -5,23 +5,6 @@ import (
 	"net/http"
 )
 
-func main() {
-	const filepathRoot = "."
-	const port = "8080"
-
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir(filepathRoot))
-	mux.Handle("/", fileServer)
-	corsMux := middlewareCors(mux)
-
-	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: corsMux,
-	}
-	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(srv.ListenAndServe())
-}
-
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -33,4 +16,29 @@ func middlewareCors(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func healthzHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset = utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir(filepathRoot))
+	mux.Handle("/", fileServer)
+	mux.HandleFunc("/healthz", healthzHandlerFunc)
+
+	corsMux := middlewareCors(mux)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
